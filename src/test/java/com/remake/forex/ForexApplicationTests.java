b08e300f0b0815a352de815987ac7f493c51beb2
Yee <yeechoong97@gmail.com>
2022-10-21 21:27:27 +0800

    First Commit

---
+++ src/test/java/com/remake/forex/ForexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remake.forex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
